bfe66278c15bb655e08c8abc21569909defc8fbe
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-08-04 13:02:16 +0200

    😺 sol interface for CreateAddress
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

---
+++ test/utils/ICreateAddress.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: WTFPL
+pragma solidity ^0.8.15;
+
+interface ICreateAddress {
+    function compute_address_rlp_self(uint256 nonce)
+        external
+        view
+        returns (address);
+
+    function compute_address_rlp(address deployer, uint256 nonce)
+        external
+        view
+        returns (address);
+}
