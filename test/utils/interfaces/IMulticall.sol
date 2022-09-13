24e17dad74183764fe968b006755a5de120e3c14
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-09-13 13:42:02 +0200

    📋 multicall interface
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

---
+++ test/utils/interfaces/IMulticall.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: WTFPL
+pragma solidity ^0.8.17;
+
+interface IMulticall {
+    struct Batch {
+        address target;
+        bool allowFailure;
+        bytes callData;
+    }
+
+    struct BatchValue {
+        address target;
+        bool allowFailure;
+        uint256 value;
+        bytes callData;
+    }
+
+    struct BatchSelf {
+        bool allowFailure;
+        bytes callData;
+    }
+
+    struct BatchValueSelf {
+        bool allowFailure;
+        uint256 value;
+        bytes callData;
+    }
+
+    struct Result {
+        bool success;
+        bytes returnData;
+    }
+
+    function multicall(Batch[] calldata batch)
+        external
+        returns (Result[] memory results);
+
+    function multicall_value(BatchValue[] calldata batchValue)
+        external
+        payable
+        returns (Result[] memory results);
+
+    function multicall_self(BatchSelf[] calldata batchSelf)
+        external
+        returns (Result[] memory results);
+
+    function multicall_value_self(BatchValueSelf[] calldata batchValueSelf)
+        external
+        payable
+        returns (Result[] memory results);
+
+    function multistaticcall(Batch[] calldata batch)
+        external
+        pure
+        returns (Result[] memory results);
+}
