9b690d297618eecaf960c27262db6dcdeb167137
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-11-14 19:55:58 +0100

    ♻️  bump merkletreejs
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/utils/scripts/generate-bad-proof.js
+++ test/utils/scripts/generate-bad-proof.js
@@ -4,7 +4,7 @@ const ethers = require("ethers");
 const badElements = ["d", "e", "f"];
 const badMerkleTree = new MerkleTree(badElements);
 
-const badProof = badMerkleTree.getHexProof(badElements[2], 2);
+const badProof = badMerkleTree.getHexProof(badElements[0]);
 
 // eslint-disable-next-line no-undef
 process.stdout.write(
