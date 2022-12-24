4a49f815e99c56f026790793618de1107ac91f2c
JT <Jtriley15@gmail.com>
2022-12-23 18:18:14 -0600

    Update test/tokens/interfaces/IERC1155Extended.sol
    Co-authored-by: Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
    Signed-off-by: JT <Jtriley15@gmail.com>

--- test/tokens/interfaces/IERC1155Extended.sol
+++ test/tokens/interfaces/IERC1155Extended.sol
@@ -5,7 +5,7 @@ import {IERC1155MetadataURI} from "openzeppelin/token/ERC1155/extensions/IERC115
 
 interface IERC1155Extended is IERC1155MetadataURI {
     function is_minter(address account) external view returns (bool);
-    function safe_mint(address owner, uint256 id, uint256 amount, bytes calldata data) external;
+    function safe_mint(address owner, uint256 id, uint256 amount, bytes memory data) external;
     function set_minter(address minter, bool status) external;
     function owner() external view returns (address);
     function transfer_ownership(address new_owner) external;
