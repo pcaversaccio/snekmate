599bcb9d88d80e6455150d97fd79071414f86658
rik61072@gmail.com <rik61072@gmail.com>
2023-02-04 06:28:07 +0530

    fuzz tests for src/utils/Create2Address.vy

--- test/utils/Create2Address.t.sol
+++ test/utils/Create2Address.t.sol
@@ -74,4 +74,72 @@ contract Create2AddressTest is Test {
             );
         assertEq(create2AddressComputed, create2AddressOZComputed);
     }
+
+    function testFuzzComputeAddress(
+        bytes32 salt,
+        address deployer
+    ) public {
+        vm.assume(deployer != address(0));
+        string memory arg1 = "MyToken";
+        string memory arg2 = "MTKN";
+        address arg3 = vm.addr(1);
+        uint256 arg4 = 100;
+        bytes memory args = abi.encode(arg1, arg2, arg3, arg4);
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("ERC20Mock.sol:ERC20Mock"),
+            args
+        );
+        bytes32 bytecodeHash = keccak256(bytecode);
+        address create2AddressComputed = create2Address.compute_address(
+            salt,
+            bytecodeHash,
+            deployer
+        );
+        vm.prank(deployer);
+        ERC20Mock create2AddressComputedOnChain = new ERC20Mock{salt: salt}(
+            arg1,
+            arg2,
+            arg3,
+            arg4
+        );
+        assertEq(
+            create2AddressComputed,
+            address(create2AddressComputedOnChain)
+        );
+    }
+
+    function testFuzzComputeAddressSelf(bytes32 salt) public {
+        string memory arg1 = "MyToken";
+        string memory arg2 = "MTKN";
+        address arg3 = vm.addr(1);
+        uint256 arg4 = 100;
+        bytes memory args = abi.encode(arg1, arg2, arg3, arg4);
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("ERC20Mock.sol:ERC20Mock"),
+            args
+        );
+        bytes32 bytecodeHash = keccak256(bytecode);
+        address create2AddressComputed = create2Address.compute_address_self(
+            salt,
+            bytecodeHash
+        );
+        address create2AddressOZComputed = create2Impl
+            .computeAddressWithDeployer(
+                salt,
+                bytecodeHash,
+                address(create2Address)
+            );
+        vm.prank(address(create2Address));
+        ERC20Mock create2AddressComputedOnChain = new ERC20Mock{salt: salt}(
+            arg1,
+            arg2,
+            arg3,
+            arg4
+        );
+        assertEq(
+            create2AddressComputed,
+            address(create2AddressComputedOnChain)
+        );
+        assertEq(create2AddressComputed, create2AddressOZComputed);
+    }
 }
