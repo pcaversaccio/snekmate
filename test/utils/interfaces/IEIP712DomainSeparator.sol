a64a6426c27625f1b1e872c6f4c41c21972174e4
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-06-06 13:31:47 +0200

    ♻️  refactor interface definition
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/utils/interfaces/IEIP712DomainSeparator.sol
+++ test/utils/interfaces/IEIP712DomainSeparator.sol
@@ -1,23 +1,12 @@
 // SPDX-License-Identifier: WTFPL
 pragma solidity ^0.8.20;
 
-interface IEIP712DomainSeparator {
+import {IERC5267} from "openzeppelin/interfaces/IERC5267.sol";
+
+interface IEIP712DomainSeparator is IERC5267 {
     function domain_separator_v4() external view returns (bytes32);
 
     function hash_typed_data_v4(
         bytes32 structHash
     ) external view returns (bytes32);
-
-    function eip712Domain()
-        external
-        view
-        returns (
-            bytes1,
-            string calldata,
-            string calldata,
-            uint256,
-            address,
-            bytes32,
-            uint256[] calldata
-        );
 }
