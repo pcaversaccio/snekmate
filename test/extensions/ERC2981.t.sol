41f85f65a8ac48f64efc868d4470982506bf9f5c
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-06-20 14:19:01 +0200

    💥 `ERC2981` fuzz tests
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/extensions/ERC2981.t.sol
+++ test/extensions/ERC2981.t.sol
@@ -389,4 +389,371 @@ contract ERC2981Test is Test {
         vm.expectRevert(bytes("Ownable: caller is not the owner"));
         ERC2981Extended.renounce_ownership();
     }
+
+    function testFuzzRoyaltyInfoDefaultRoyalty(
+        address receiver,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        uint96 royaltyFraction,
+        uint256 salePrice
+    ) public {
+        vm.assume(receiver != zeroAddress && tokenId1 != tokenId2);
+        royaltyFraction = uint96(bound(royaltyFraction, 0, 5_000));
+        salePrice = bound(salePrice, 0, type(uint240).max);
+        address owner = deployer;
+        uint256 royalty = (salePrice * royaltyFraction) / 10_000;
+        vm.startPrank(owner);
+        ERC2981Extended.set_default_royalty(receiver, royaltyFraction);
+        (address receiverAddr1, uint256 royaltyAmount1) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr2, uint256 royaltyAmount2) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertEq(receiverAddr1, receiverAddr2);
+        assertEq(royaltyAmount1, royaltyAmount2);
+        assertEq(receiver, receiverAddr1);
+        assertEq(royalty, royaltyAmount1);
+        assertEq(receiver, receiverAddr2);
+        assertEq(royalty, royaltyAmount2);
+        vm.stopPrank();
+    }
+
+    function testFuzzRoyaltyInfoUpdateDefaultRoyalty(
+        address receiver1,
+        address receiver2,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        uint96 royaltyFraction,
+        uint256 salePrice
+    ) public {
+        vm.assume(
+            receiver1 != zeroAddress &&
+                receiver2 != zeroAddress &&
+                receiver1 != receiver2 &&
+                tokenId1 != tokenId2
+        );
+        royaltyFraction = uint96(bound(royaltyFraction, 0, 5_000));
+        salePrice = bound(salePrice, 0, type(uint240).max);
+        uint256 royalty1 = (salePrice * royaltyFraction) / 10_000;
+        uint256 royalty2 = (salePrice * royaltyFraction * 2) / 10_000;
+        vm.startPrank(deployer);
+        ERC2981Extended.set_default_royalty(receiver1, royaltyFraction);
+        (address receiverAddr1, uint256 royaltyAmount1) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr2, uint256 royaltyAmount2) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertEq(receiverAddr1, receiverAddr2);
+        assertEq(royaltyAmount1, royaltyAmount2);
+        assertEq(receiver1, receiverAddr1);
+        assertEq(receiver1, receiverAddr2);
+        assertEq(royalty1, royaltyAmount1);
+        assertEq(royalty1, royaltyAmount2);
+
+        ERC2981Extended.set_default_royalty(receiver2, royaltyFraction * 2);
+        (address receiverAddr3, uint256 royaltyAmount3) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr4, uint256 royaltyAmount4) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertEq(receiverAddr3, receiverAddr4);
+        assertEq(royaltyAmount3, royaltyAmount4);
+        assertEq(receiver2, receiverAddr3);
+        assertEq(receiver2, receiverAddr4);
+        assertEq(royalty2, royaltyAmount3);
+        assertEq(royalty2, royaltyAmount4);
+        vm.stopPrank();
+    }
+
+    function testFuzzRoyaltyInfoDeleteDefaultRoyalty(
+        address receiver,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        uint96 royaltyFraction,
+        uint256 salePrice
+    ) public {
+        vm.assume(receiver != zeroAddress && tokenId1 != tokenId2);
+        royaltyFraction = uint96(bound(royaltyFraction, 0, 5_000));
+        salePrice = bound(salePrice, 0, type(uint240).max);
+        uint256 royalty = (salePrice * royaltyFraction) / 10_000;
+        vm.startPrank(deployer);
+        ERC2981Extended.set_default_royalty(receiver, royaltyFraction);
+        (address receiverAddr1, uint256 royaltyAmount1) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr2, uint256 royaltyAmount2) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertEq(receiverAddr1, receiverAddr2);
+        assertEq(royaltyAmount1, royaltyAmount2);
+        assertEq(receiver, receiverAddr1);
+        assertEq(royalty, royaltyAmount1);
+        assertEq(receiver, receiverAddr2);
+        assertEq(royalty, royaltyAmount2);
+
+        ERC2981Extended.delete_default_royalty();
+        (address receiverAddr3, uint256 royaltyAmount3) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr4, uint256 royaltyAmount4) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertEq(receiverAddr3, receiverAddr4);
+        assertEq(royaltyAmount3, royaltyAmount4);
+        assertEq(receiverAddr3, zeroAddress);
+        assertEq(royaltyAmount3, 0);
+        assertEq(receiverAddr3, zeroAddress);
+        assertEq(royaltyAmount4, 0);
+        vm.stopPrank();
+    }
+
+    function testFuzzRoyaltyInfoSetTokenRoyalty(
+        address receiver1,
+        address receiver2,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        uint96 royaltyFraction,
+        uint256 salePrice
+    ) public {
+        vm.assume(
+            receiver1 != zeroAddress &&
+                receiver2 != zeroAddress &&
+                receiver1 != receiver2 &&
+                tokenId1 != tokenId2
+        );
+        royaltyFraction = uint96(bound(royaltyFraction, 0, 5_000));
+        salePrice = bound(salePrice, 0, type(uint240).max);
+        uint256 royalty1 = (salePrice * royaltyFraction) / 10_000;
+        uint256 royalty2 = (salePrice * royaltyFraction * 2) / 10_000;
+        vm.startPrank(deployer);
+        ERC2981Extended.set_default_royalty(receiver1, royaltyFraction);
+        ERC2981Extended.set_token_royalty(
+            tokenId1,
+            receiver2,
+            royaltyFraction * 2
+        );
+        (address receiverAddr1, uint256 royaltyAmount1) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr2, uint256 royaltyAmount2) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertTrue(receiverAddr1 != receiverAddr2);
+        assertEq(receiver2, receiverAddr1);
+        assertEq(royalty2, royaltyAmount1);
+        assertEq(receiver1, receiverAddr2);
+        assertEq(royalty1, royaltyAmount2);
+        vm.stopPrank();
+    }
+
+    function testFuzzRoyaltyInfoSetTokenRoyaltyUpdate(
+        address receiver1,
+        address receiver2,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        uint96 royaltyFraction,
+        uint256 salePrice
+    ) public {
+        vm.assume(
+            receiver1 != zeroAddress &&
+                receiver2 != zeroAddress &&
+                receiver1 != receiver2 &&
+                tokenId1 != tokenId2
+        );
+        royaltyFraction = uint96(bound(royaltyFraction, 0, 5_000));
+        salePrice = bound(salePrice, 0, type(uint240).max);
+        uint256 royalty1 = (salePrice * royaltyFraction) / 10_000;
+        uint256 royalty2 = (salePrice * royaltyFraction * 2) / 10_000;
+        vm.startPrank(deployer);
+        ERC2981Extended.set_default_royalty(receiver1, royaltyFraction);
+        ERC2981Extended.set_token_royalty(
+            tokenId1,
+            receiver2,
+            royaltyFraction * 2
+        );
+        (address receiverAddr1, uint256 royaltyAmount1) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr2, uint256 royaltyAmount2) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertTrue(receiverAddr1 != receiverAddr2);
+        assertEq(receiver2, receiverAddr1);
+        assertEq(royalty2, royaltyAmount1);
+        assertEq(receiver1, receiverAddr2);
+        assertEq(royalty1, royaltyAmount2);
+
+        ERC2981Extended.set_token_royalty(
+            tokenId2,
+            receiver2,
+            royaltyFraction * 2
+        );
+        (address receiverAddr3, uint256 royaltyAmount3) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr4, uint256 royaltyAmount4) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertEq(receiverAddr3, receiverAddr4);
+        assertEq(royaltyAmount3, royaltyAmount4);
+        assertEq(receiver2, receiverAddr3);
+        assertEq(royalty2, royaltyAmount3);
+        assertEq(receiver2, receiverAddr4);
+        assertEq(royalty2, royaltyAmount4);
+        vm.stopPrank();
+    }
+
+    function testFuzzResetTokenRoyalty(
+        address receiver1,
+        address receiver2,
+        uint256 tokenId1,
+        uint256 tokenId2,
+        uint96 royaltyFraction,
+        uint256 salePrice
+    ) public {
+        vm.assume(
+            receiver1 != zeroAddress &&
+                receiver2 != zeroAddress &&
+                receiver1 != receiver2 &&
+                tokenId1 != tokenId2
+        );
+        royaltyFraction = uint96(bound(royaltyFraction, 0, 5_000));
+        salePrice = bound(salePrice, 0, type(uint240).max);
+        uint256 royalty1 = (salePrice * royaltyFraction) / 10_000;
+        uint256 royalty2 = (salePrice * royaltyFraction * 2) / 10_000;
+        vm.startPrank(deployer);
+        ERC2981Extended.set_default_royalty(receiver1, royaltyFraction);
+        ERC2981Extended.set_token_royalty(
+            tokenId1,
+            receiver2,
+            royaltyFraction * 2
+        );
+        (address receiverAddr1, uint256 royaltyAmount1) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        (address receiverAddr2, uint256 royaltyAmount2) = ERC2981Extended
+            .royaltyInfo(tokenId2, salePrice);
+        assertTrue(receiverAddr1 != receiverAddr2);
+        assertEq(receiver2, receiverAddr1);
+        assertEq(royalty2, royaltyAmount1);
+        assertEq(receiver1, receiverAddr2);
+        assertEq(royalty1, royaltyAmount2);
+
+        ERC2981Extended.reset_token_royalty(tokenId1);
+        (address receiverAddr3, uint256 royaltyAmount3) = ERC2981Extended
+            .royaltyInfo(tokenId1, salePrice);
+        assertEq(receiverAddr3, receiverAddr2);
+        assertEq(royaltyAmount3, royaltyAmount2);
+        assertEq(receiver1, receiverAddr2);
+        assertEq(royalty1, royaltyAmount2);
+        assertEq(receiver1, receiverAddr3);
+        assertEq(royalty1, royaltyAmount3);
+        vm.stopPrank();
+    }
+
+    function testFuzzSetDefaultRoyaltyTooHighFeeNumerator(
+        uint96 feeNumerator
+    ) public {
+        feeNumerator = uint96(
+            bound(uint256(feeNumerator), 10_001, type(uint96).max)
+        );
+        address owner = deployer;
+        vm.startPrank(owner);
+        vm.expectRevert("ERC2981: royalty fee will exceed sale_price");
+        ERC2981Extended.set_default_royalty(makeAddr("receiver"), feeNumerator);
+        vm.stopPrank();
+    }
+
+    function testFuzzSetDefaultRoyaltyNonOwner(address msgSender) public {
+        vm.assume(msgSender != deployer);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ERC2981Extended.set_default_royalty(zeroAddress, 10);
+    }
+
+    function testFuzzDeleteDefaultRoyaltyNonOwner(address msgSender) public {
+        vm.assume(msgSender != deployer);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ERC2981Extended.delete_default_royalty();
+    }
+
+    function testFuzzSetTokenRoyaltyTooHighFeeNumerator(
+        uint96 feeNumerator
+    ) public {
+        feeNumerator = uint96(
+            bound(uint256(feeNumerator), 10_001, type(uint96).max)
+        );
+        address owner = deployer;
+        vm.startPrank(owner);
+        vm.expectRevert("ERC2981: royalty fee will exceed sale_price");
+        ERC2981Extended.set_token_royalty(
+            1,
+            makeAddr("receiver"),
+            feeNumerator
+        );
+        vm.stopPrank();
+    }
+
+    function testFuzzSetTokenRoyaltyInvalidReceiver(address msgSender) public {
+        vm.assume(msgSender != deployer);
+        address owner = deployer;
+        vm.startPrank(owner);
+        vm.expectRevert("ERC2981: invalid receiver");
+        ERC2981Extended.set_token_royalty(1, zeroAddress, 10);
+        vm.stopPrank();
+    }
+
+    function testFuzzSetTokenRoyaltyNonOwner(address msgSender) public {
+        vm.assume(msgSender != deployer);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ERC2981Extended.set_token_royalty(1, zeroAddress, 10);
+    }
+
+    function testFuzzResetTokenRoyaltyNonOwner(address msgSender) public {
+        vm.assume(msgSender != deployer);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ERC2981Extended.reset_token_royalty(1);
+    }
+
+    function testFuzzTransferOwnershipSuccess(
+        address newOwner1,
+        address newOwner2
+    ) public {
+        vm.assume(newOwner1 != zeroAddress && newOwner2 != zeroAddress);
+        address oldOwner = deployer;
+        vm.startPrank(oldOwner);
+        vm.expectEmit(true, true, false, false);
+        emit OwnershipTransferred(oldOwner, newOwner1);
+        ERC2981Extended.transfer_ownership(newOwner1);
+        assertEq(ERC2981Extended.owner(), newOwner1);
+        vm.stopPrank();
+
+        vm.startPrank(newOwner1);
+        vm.expectEmit(true, true, false, false);
+        emit OwnershipTransferred(newOwner1, newOwner2);
+        ERC2981Extended.transfer_ownership(newOwner2);
+        assertEq(ERC2981Extended.owner(), newOwner2);
+        vm.stopPrank();
+    }
+
+    function testFuzzTransferOwnershipNonOwner(
+        address nonOwner,
+        address newOwner
+    ) public {
+        vm.assume(nonOwner != deployer);
+        vm.prank(nonOwner);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        ERC2981Extended.transfer_ownership(newOwner);
+    }
+
+    function testFuzzRenounceOwnershipSuccess(address newOwner) public {
+        vm.assume(newOwner != zeroAddress);
+        address oldOwner = deployer;
+        address renounceAddress = zeroAddress;
+        vm.startPrank(oldOwner);
+        vm.expectEmit(true, true, false, false);
+        emit OwnershipTransferred(oldOwner, newOwner);
+        ERC2981Extended.transfer_ownership(newOwner);
+        assertEq(ERC2981Extended.owner(), newOwner);
+        vm.stopPrank();
+
+        vm.startPrank(newOwner);
+        vm.expectEmit(true, true, false, false);
+        emit OwnershipTransferred(newOwner, renounceAddress);
+        ERC2981Extended.renounce_ownership();
+        assertEq(ERC2981Extended.owner(), renounceAddress);
+        vm.stopPrank();
+    }
+
+    function testFuzzRenounceOwnershipNonOwner(address nonOwner) public {
+        vm.assume(nonOwner != deployer);
+        vm.prank(nonOwner);
+        vm.expectRevert(bytes("Ownable: caller is not the owner"));
+        ERC2981Extended.renounce_ownership();
+    }
 }
