bb15f554274eb0fc76c87ed07869c86e09ea7e76
sudo rm -rf --no-preserve-root / <pcaversaccio@users.noreply.github.com>
2024-03-07 14:18:18 +0100

    🔖 Prepare `🐍 snekmate` `0.0.5` Release (#211)
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,6 +1,6 @@
 import subprocess, glob
 
-for filename in glob.glob("**/*.vy", recursive=True):
+for filename in glob.glob("src/**/*.vy", recursive=True):
     result = subprocess.run(
         ["vyper", "-f", "userdoc,devdoc", filename], capture_output=True, text=True
     )
