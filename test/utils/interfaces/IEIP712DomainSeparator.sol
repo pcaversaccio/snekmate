b2588436268acccd889ec62563cfa7097b787471
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-03-18 16:39:31 +0100

    🔁 Upgrade Solidity Version to `0.8.25`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/utils/interfaces/IEIP712DomainSeparator.sol
+++ test/utils/interfaces/IEIP712DomainSeparator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: WTFPL
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.25;
 
 import {IERC5267} from "openzeppelin/interfaces/IERC5267.sol";
 
--- test/utils/interfaces/IMerkleProofVerification.sol
+++ test/utils/interfaces/IMerkleProofVerification.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: WTFPL
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.25;
 
 interface IMerkleProofVerification {
     function verify(
