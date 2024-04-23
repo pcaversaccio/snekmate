ac8daa3a1a3ab845a9a58bf4d9fda5ce5fda1e27
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-04-23 15:37:29 +0200

    ♻️  Refactor `compile` Script
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- lib/utils/compile.py
+++ lib/utils/compile.py
@@ -1,17 +1,8 @@
 import sys, subprocess
 
-path = sys.argv[1]
-
-if len(sys.argv) > 2:
-    # If the EVM version is configured.
-    result = subprocess.run(
-        ["vyper", path, sys.argv[2], sys.argv[3]], capture_output=True, text=True
-    )
-else:
-    result = subprocess.run(["vyper", path], capture_output=True, text=True)
-
+result = subprocess.run(["vyper"] + sys.argv[1:], capture_output=True, text=True)
 if result.stderr != "":
-    raise Exception("Error compiling: " + path)
+    raise Exception("Error compiling: " + sys.argv[1])
 
 # Remove any leading and trailing whitespace characters
 # from the compilation result.
