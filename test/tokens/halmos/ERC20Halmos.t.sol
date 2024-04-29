7d001643564a9393e8bcea1d9711feba248e0ca4
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-04-29 20:59:25 +0200

    ♻️  Refactor `ERC20` Halmos test
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/tokens/halmos/ERC20Halmos.t.sol
+++ test/tokens/halmos/ERC20Halmos.t.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: WTFPL
 pragma solidity ^0.8.25;
 
+import {Test} from "forge-std/Test.sol";
+import {SymTest} from "halmos-cheatcodes/SymTest.sol";
 import {VyperDeployer} from "utils/VyperDeployer.sol";
 
-import {IERC20, ERC20Test} from "./ERC20Test.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 
 /**
  * @dev Sets the timeout (in milliseconds) for solving assertion
  * violation conditions; `0` means no timeout.
  * @custom:halmos --solver-timeout-assertion 0
  */
-contract ERC20TestHalmos is ERC20Test {
+contract ERC20TestHalmos is Test, SymTest {
     string private constant _NAME = "MyToken";
     string private constant _SYMBOL = "WAGMI";
     uint8 private constant _DECIMALS = 18;
@@ -20,12 +22,16 @@ contract ERC20TestHalmos is ERC20Test {
 
     VyperDeployer private vyperDeployer = new VyperDeployer();
 
+    IERC20 private erc20;
+    address private token;
+    address[] private holders;
+
     /**
      * @dev Sets timeout (in milliseconds) for solving branching
      * conditions; `0` means no timeout.
      * @custom:halmos --solver-timeout-branching 1000
      */
-    function setUp() public override {
+    function setUp() public {
         bytes memory args = abi.encode(
             _NAME,
             _SYMBOL,
@@ -74,12 +80,104 @@ contract ERC20TestHalmos is ERC20Test {
         }
     }
 
+    /**
+     * @notice Forked and adjusted accordingly from here:
+     * https://github.com/a16z/halmos/blob/main/examples/tokens/ERC20/test/ERC20Test.sol.
+     */
     function testHalmosAssertNoBackdoor(
         bytes4 selector,
         address caller,
         address other
     ) public {
         bytes memory args = svm.createBytes(1_024, "WAGMI");
-        checkNoBackdoor(selector, args, caller, other);
+        vm.assume(other != caller);
+
+        uint256 oldBalanceOther = erc20.balanceOf(other);
+        uint256 oldAllowance = erc20.allowance(other, caller);
+
+        vm.startPrank(caller);
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, ) = token.call(abi.encodePacked(selector, args));
+        vm.assume(success);
+        vm.stopPrank();
+
+        uint256 newBalanceOther = erc20.balanceOf(other);
+
+        if (newBalanceOther < oldBalanceOther) {
+            assert(oldAllowance >= oldBalanceOther - newBalanceOther);
+        }
+    }
+
+    /**
+     * @notice Forked and adjusted accordingly from here:
+     * https://github.com/a16z/halmos/blob/main/examples/tokens/ERC20/test/ERC20Test.sol.
+     */
+    function testHalmosTransfer(
+        address sender,
+        address receiver,
+        address other,
+        uint256 amount
+    ) public {
+        vm.assume(other != sender && other != receiver);
+
+        uint256 oldBalanceSender = erc20.balanceOf(sender);
+        uint256 oldBalanceReceiver = erc20.balanceOf(receiver);
+        uint256 oldBalanceOther = erc20.balanceOf(other);
+
+        vm.startPrank(sender);
+        erc20.transfer(receiver, amount);
+        vm.stopPrank();
+
+        if (sender != receiver) {
+            assert(erc20.balanceOf(sender) <= oldBalanceSender);
+            assert(erc20.balanceOf(sender) == oldBalanceSender - amount);
+            assert(erc20.balanceOf(receiver) >= oldBalanceReceiver);
+            assert(erc20.balanceOf(receiver) == oldBalanceReceiver + amount);
+        } else {
+            assert(erc20.balanceOf(sender) == oldBalanceSender);
+            assert(erc20.balanceOf(receiver) == oldBalanceReceiver);
+        }
+
+        assert(erc20.balanceOf(other) == oldBalanceOther);
+    }
+
+    /**
+     * @notice Forked and adjusted accordingly from here:
+     * https://github.com/a16z/halmos/blob/main/examples/tokens/ERC20/test/ERC20Test.sol.
+     */
+    function testHalmosTransferFrom(
+        address caller,
+        address from,
+        address to,
+        address other,
+        uint256 amount
+    ) public {
+        vm.assume(other != from && other != to);
+
+        uint256 oldBalanceFrom = erc20.balanceOf(from);
+        uint256 oldBalanceTo = erc20.balanceOf(to);
+        uint256 oldBalanceOther = erc20.balanceOf(other);
+        uint256 oldAllowance = erc20.allowance(from, caller);
+
+        vm.startPrank(caller);
+        erc20.transferFrom(from, to, amount);
+        vm.stopPrank();
+
+        if (from != to) {
+            assert(erc20.balanceOf(from) <= oldBalanceFrom);
+            assert(erc20.balanceOf(from) == oldBalanceFrom - amount);
+            assert(erc20.balanceOf(to) >= oldBalanceTo);
+            assert(erc20.balanceOf(to) == oldBalanceTo + amount);
+            assert(oldAllowance >= amount);
+            assert(
+                oldAllowance == type(uint256).max ||
+                    erc20.allowance(from, caller) == oldAllowance - amount
+            );
+        } else {
+            assert(erc20.balanceOf(from) == oldBalanceFrom);
+            assert(erc20.balanceOf(to) == oldBalanceTo);
+        }
+
+        assert(erc20.balanceOf(other) == oldBalanceOther);
     }
 }
