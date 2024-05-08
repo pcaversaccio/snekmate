3e02a552c4494bdc0e1ab379a1b4f75e4bbd4abf
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-05-08 21:36:29 +0200

    ♻️  Testing what is wrong
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- lib/utils/compile.py
+++ lib/utils/compile.py
@@ -1,8 +1,8 @@
 import sys, subprocess
 
 result = subprocess.run(["vyper"] + sys.argv[1:], capture_output=True, text=True)
-if result.returncode != 0:
-    raise Exception("Error compiling: " + sys.argv[1])
+# if result.returncode != 0:
+#     raise Exception("Error compiling: " + sys.argv[1])
 
 # Remove any leading and trailing whitespace characters
 # from the compilation result.
