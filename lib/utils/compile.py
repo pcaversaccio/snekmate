6f65503d54f27eb1dcafdf0c1cc08a4aab596cec
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-10-07 17:38:47 +0200

    ♻️  Refactor
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- lib/utils/compile.py
+++ lib/utils/compile.py
@@ -1,8 +1,30 @@
 import sys, subprocess
 
-result = subprocess.run(["vyper"] + sys.argv[1:], capture_output=True, text=True)
+
+# Check if `experimental_codegen` is enabled in the
+# Foundry profile.
+def is_experimental_codegen():
+    return (
+        subprocess.run(
+            ["bash", "-c", 'forge config --json | jq -r ".vyper.experimental_codegen"'],
+            capture_output=True,
+            text=True,
+        )
+        .stdout.strip()
+        .lower()
+        == "true"
+    )
+
+
+# Build the Vyper command.
+command = (
+    ["vyper", "--experimental-codegen"] if is_experimental_codegen() else ["vyper"]
+)
+command += sys.argv[1:]
+
+result = subprocess.run(command, capture_output=True, text=True)
 if result.returncode != 0:
-    raise Exception("Error compiling: " + sys.argv[1])
+    raise Exception(f"Error compiling: {sys.argv[1]}")
 
 # Remove any leading and trailing whitespace characters
 # from the compilation result.
