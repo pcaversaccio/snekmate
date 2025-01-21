8d5ba23676fc1c6141f35f592aa341c6dbe9ac76
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2025-01-21 16:58:08 +0100

    ♻️ Interface Functions Prettify
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/utils/interfaces/IPausable.sol
+++ test/utils/interfaces/IPausable.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.28;
 
 interface IPausable {
     event Paused(address account);
+
     event Unpaused(address account);
+
     function paused() external view returns (bool);
+
     function pause() external;
+
     function unpause() external;
 }
