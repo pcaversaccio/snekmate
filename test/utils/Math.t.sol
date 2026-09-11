8807a07289d7f148a6fe225630fc9840af062068
pcaversaccio <pascal.caversaccio@hotmail.ch>
2026-09-11 16:12:27 +0200

    ♻️ Fix `Math.t.sol`
    Signed-off-by: pcaversaccio <pascal.caversaccio@hotmail.ch>

--- test/utils/Math.t.sol
+++ test/utils/Math.t.sol
@@ -305,7 +305,7 @@ contract MathTest is Test {
     function testWadExp() public view {
         assertEq(math.wad_exp(-41_446_531_673_892_822_313), 0);
         assertEq(math.wad_exp(-41_446_531_673_892_822_312), 1);
-        assertEq(math.wad_exp(-3 * 10 ** 18), 49_787_0.8.377_863_942);
+        assertEq(math.wad_exp(-3 * 10 ** 18), 49_787_068_367_863_942);
         assertEq(math.wad_exp(-2 * 10 ** 18), 135_335_283_236_612_691);
         assertEq(math.wad_exp(-1 * 10 ** 18), 367_879_441_171_442_321);
         assertEq(math.wad_exp(-0.5 * 10 ** 18), 606_530_659_712_633_423);
